d77ad0c46c0a455fac826db48169d684c5eb50f3
Elisa <elisalok413@gmail.com>
2025-01-06 20:01:57 +1300

    add User Model

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+namespace BlogApi.Models;
+
+public class User 
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+
+  public string Email { get; set; } = string.Empty;
+
+  public string Password { get; set; } = string.Empty;
+
+  public string Role { get; set; } = string.Empty;
+
+  public string PhoneNumber { get; set; } = string.Empty;
+
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+  public string Status { get; set; } = string.Empty;
+}
--- obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22367dd0d038ce107dbcccba6648c2a3c5f3cdae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4af76629c0c39ec0341da6740b5dfc7465ad8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
