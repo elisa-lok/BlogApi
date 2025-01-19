6202d9b42e7e50d3d448ef3c0ba624b0581542cd
Elisa <elisalok413@gmail.com>
2025-01-19 19:59:54 +1300

    update dbContext

--- Data/BlogContext.cs
+++ Data/BlogContext.cs
@@ -12,7 +12,7 @@ namespace BlogApi.Data
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<Post> Posts { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
-        public DbSet<RequestLog> RequestLogs { get; set; }
+        public DbSet<RequestLog> RequestLogs { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
